3cd3f1a50b8fe27dbd33330630d1f01897bf1916
Boburshoh-oss <Boburbekbotirov868@gmail.com>
2024-09-09 05:22:02 +0500

    migrations

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -5,12 +5,12 @@ class Projects(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField(upload_to='projects/')
-    image2 = models.ImageField(upload_to='projects/')
-    image3 = models.ImageField(upload_to='projects/')
-    image4 = models.ImageField(upload_to='projects/')
-    image5 = models.ImageField(upload_to='projects/')
+    image2 = models.ImageField(upload_to='projects/',blank=True,null=True)
+    image3 = models.ImageField(upload_to='projects/',blank=True,null=True)
+    image4 = models.ImageField(upload_to='projects/',blank=True,null=True)
+    image5 = models.ImageField(upload_to='projects/',blank=True,null=True)
 
-    link = models.URLField(max_length=255)
+    link = models.URLField(max_length=255,blank=True,null=True)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
